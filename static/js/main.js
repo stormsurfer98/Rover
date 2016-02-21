3ed0623e016b071baad1813f51614271f41b603a
Shrey Gupta <gupta.lego@gmail.com>
2016-02-21 00:22:50 -0500

    General commit

--- static/js/main.js
+++ static/js/main.js
@@ -61,7 +61,7 @@ function findFlight() {
         "refundable": false
       }
     };
-    $.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBHUfsS5k8fWdr6V_151x2kFKoRgTUx_Io", passInfo, function(data) {
+    $.get("https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBHUfsS5k8fWdr6V_151x2kFKoRgTUx_Io", passInfo, function(data) {
       var departureTime1 = formatString(data["tripOption"][0]["slice"][0]["segment"]["leg"]["departureTime"]);
       var arrivalTime1 = formatString(data["tripOption"][0]["slice"][0]["segment"]["leg"]["arrivalTime"]);
       var startTime = departureTime1 + " to " + arrivalTime1;
